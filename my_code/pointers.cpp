c4ada6e76767ecb957dbb74dd7468088a230f3d7
Max Viduszynski <mav456@nyu.edu>
2019-06-07 22:55:42 -0400

    update

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -3,47 +3,29 @@
 
 using namespace std;
 
-/*
- * Increment n.
- * */
 void inc_by_value(int n) {
     ++n;
-    
 }
 
 
-
-/*
- * Increment n.
- * */
 void inc_with_pointer(int* iptr) {
     ++(*iptr);
 }
 
 
-/*
- * Increment n.
- * */
 void inc_with_reference(int& n) {
     ++n;
 }
 
 
-/*
- * Create an array of n things, each one with its val set to
- * its index in the Thing array.
- * */
 Thing** create_array_of_things(int n) {
-    Thing** arr = new Thing*[n];
-    for(int i=0; i < n; i++){
-        arr[i] = new Thing(i);
+    Thing** array = new Thing*[n];
+    for (int i = 0; i < n; i++) {
+        array[i] = new Thing(i);
     }
-    return arr;
+    return array;
 }
 
-/*
- * Print all of the things created in the function above.
- * */
 void print_all_things(Thing** things, int n) {
     for (int i = 0; i < n; i++) {
         cout << things[i]->val << ' ';
@@ -51,19 +33,12 @@ void print_all_things(Thing** things, int n) {
     cout << endl;
 }
 
-/*
- * Double the value of each thing (use *=).
- * */
 void double_all_things(Thing** things, int n) {
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         things[i]->val *= 2;
     }
 }
 
-
-/*
- * Delete all of the things, and the Thing array, that you have created.
- * */
 void delete_all_things(Thing** things, int n) {
     for (int i = 0; i < n; i++) {
         delete things[i];
@@ -72,17 +47,10 @@ void delete_all_things(Thing** things, int n) {
 }
 
 
-/*
- * Assign student `s` the TA `ta`.
- * */
 void assignTA(Student* s, Student* ta) {
     s->ta = ta;
 }
 
-
-/*
- * Print who has which TA.
- * */
 void printTAs(vector<Student*>& students) {
     for (Student* s: students) {
         if (s->ta == nullptr) {
