f033d78b35f9c153a52110aed44908cbf5a3135f
Max Viduszynski <mav456@nyu.edu>
2019-06-07 23:20:08 -0400

    Update pointers.cpp

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,61 +4,61 @@
 using namespace std;
 
 void inc_by_value(int n) {
-    n += 1;
+    ++n;
 }
 
 
 void inc_with_pointer(int* iptr) {
-    (*iptr)++;
+    ++(*iptr);
 }
 
 
 void inc_with_reference(int& n) {
-    n++;
+    ++n;
 }
 
 
 Thing** create_array_of_things(int n) {
-    Thing** tarr = new Thing*[n];
-    for (int a = 0 ; a < n ; a++) {
-        tarr[a] = new Thing(a);
+    Thing** arr = new Thing*[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = new Thing(i);
     }
-    return tarr;
+    return arr;
 }
 
 void print_all_things(Thing** things, int n) {
-    for (int a = 0 ; a < n ; a++) {
-        cout << things[a] -> val << " ";
+    for(int i = 0; i < n; i++){
+        cout << things[i]->val << ' ';
     }
     cout << endl;
 }
 
 void double_all_things(Thing** things, int n) {
-    for (int a = 0 ; a < n ; a++) {
-        things[a] -> val *= 2;
+    for(int i = 0; i < n; i++){
+        things[i]->val *= 2;
     }
 }
 
 void delete_all_things(Thing** things, int n) {
-    for (int a = 0 ; a < n ; a++) {
-        things[a] = nullptr;
+    for(int i = 0; i < n; i++){
+        delete things[i];
     }
-    //things = nullptr;
+    delete [] things;
 }
 
 
 void assignTA(Student* s, Student* ta) {
-    s -> ta = ta;
+    s->ta = ta;
 }
 
 void printTAs(vector<Student*>& students) {
-    for (size_t a = 0 ; a < students.size() ; a++) {
-        cout << students[a] -> name << " --> ";
-        if (students[a] -> ta != nullptr) {
-            cout << students[a] -> ta -> name << endl;
+    for(Student* s: students){
+        if(s->ta == nullptr){
+            cout << s->name << " has no TA." << endl;
         }
         else {
-            cout << "No TA" << endl;
+            cout << s->name << " has TA- " << s->ta->name << '.' << endl;
         }
     }
+    cout << endl;
 }
