c1f447ddd06249563908ce0d091b96dd6905bc82
Max Viduszynski <mav456@nyu.edu>
2019-06-11 14:05:33 -0400

    updated makerfile

--- my_code/complex.cpp
+++ my_code/complex.cpp
@@ -6,23 +6,23 @@
 using namespace std;
 
 bool operator== (const Complex& c1, const Complex& c2) {
-        return ((c1.get_imag() == c2.get_imag()) && (c1.get_real() == c2.get_real()));
+    return ((c1.get_imag() == c2.get_imag()) && (c1.get_real() == c2.get_real()));
 }
 
 bool operator!= (const Complex& c1, const Complex& c2){
-        return ((c1.get_imag() != c2.get_imag()) && (c1.get_real() != c2.get_real()));
+    return ((c1.get_imag() != c2.get_imag()) && (c1.get_real() != c2.get_real()));
 }
 
 ostream& operator<< (ostream& os, const Complex& c) {
-        /*
+    /*
               * Outputting a `Complex` instance, while illustrating some of the
               * capabilities of I/O streams: `setprecision` gives us a fixed
               * number of decimal places, while `showpos` turns on the plus
               * sign for positive numbers.
               * */
-        os << setprecision(10) << c.real << showpos
-        << c.imag << "i" << noshowpos << endl;
-        return os;
+    os << setprecision(10) << c.real << showpos
+    << c.imag << "i" << noshowpos << endl;
+    return os;
 }
 
 
@@ -30,8 +30,8 @@ ostream& operator<< (ostream& os, const Complex& c) {
  * Read a `Complex` number from an input stream.
  * */
 istream& operator>> (istream& is, Complex& c) {
-        is >> c.real >> c.imag;
-        return is;
+    is >> c.real >> c.imag;
+    return is;
 }
 
 
@@ -40,42 +40,43 @@ Complex::Complex(double real, double imag)
 
 
 Complex::operator bool() const {
-        return ((real != 0) || (imag != 0));
+    return ((real != 0) || (imag != 0));
 }
 
 Complex& Complex::operator++() {
-        ++real;
-        return (*this);
+    ++real;
+    return (*this);
 }
 
 Complex Complex::operator++(int dummy) {
-        Complex temp(*this);
-        real++;
-        return temp;
+    Complex temp(*this);
+    real++;
+    return temp;
 }
 
 Complex operator+(const Complex& c1, const Complex& c2){
-        Complex sum{c1.get_real() + c2.get_real(), c1.get_imag() + c2.get_imag()};
-        return sum;
+    Complex sum{c1.get_real() + c2.get_real(), c1.get_imag() + c2.get_imag()};
+    return sum;
 }
 
 Complex operator-(const Complex& c1, const Complex& c2){
-        Complex sub = Complex(c1.get_real() - c2.get_real(), c1.get_imag() - c2.get_imag());
-        return sub;
+    Complex sub = Complex(c1.get_real() - c2.get_real(), c1.get_imag() - c2.get_imag());
+    return sub;
 }
 
 Complex Complex::operator*(const int i){
-        real *= i;
-        return (*this);
+    real *= i;
+    return (*this);
 }
 
+Complex 
 
 
 
 double Complex::get_real() const {
-        return real;
+    return real;
 }
 
 double Complex::get_imag() const {
-        return imag;
+    return imag;
 }
