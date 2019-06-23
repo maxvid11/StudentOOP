48402553bcdf2584a976f45b92e219d040dec00f
Max Viduszynski <mav456@nyu.edu>
2019-06-22 21:14:47 -0400

    new llist

--- my_code/llist.cpp
+++ my_code/llist.cpp
@@ -5,54 +5,107 @@ using namespace std;
 
 
 
-/*
- * Output a Node pointer.
- * */
 ostream& operator<<(ostream& os, const Node* nd) {
-    os << nd->data << "\t";
+    os << nd->data;
     return os;
 }
 
 
-/*
- * Add a node to the end of a list.
- * */
-void add_at_end(Node * head, int d) {
-    if (!head->next){
-        head->next = new Node(d);
+
+void add_at_end(Node*& head, int d) {
+    if(head->next != nullptr){
+        add_at_end(head->next, d);
+        return;
+    }
+    head->next = new Node(d);
+}
+
+
+
+void print_list(ostream& os, const Node* curr) {
+    if(curr == nullptr){
+        os << "The List is Empty" << endl;;
+        return;
+    }
+    os << curr->data;
+    if(curr->next != nullptr){
+        os << " -> ";
+        print_list(os, curr->next);
+    }
+    os << endl;
+}
+
+
+void add_at_front(Node*& head, int d) {
+    Node* new_head = new Node(d);
+    new_head->next = head;
+    head = new_head;
+}
+
+bool del_tail(Node*& head) {
+    if(!head){
+        return false;
     }
     else{
-        add_at_end(head->next, d);
+        if(head->next == nullptr){
+            delete head;
+            head = nullptr;
+            return true;
+        }
+        else{
+            return del_tail(head->next);
+        }
+    }
+}
+
+bool del_head(Node*& head) {
+    if(!head){
+        return false;
+    }
+    else{
+        Node* old_head = head;
+        head = head->next;
+        delete old_head;
+        return true;
     }
 }
 
 
-/*
- * Given the head of a list, print the whole thing.
- * Let's do this recursively!
- * */
-void print_list(ostream& os, const Node* head) {
-    os << head;
-    if (head->next) print_list(os, head->next);
-    else os << endl;
+
+Node* reverse(Node* curr, Node* new_next){
+    if(!curr){
+        return new_next;
+    }
+    else{
+        return reverse(curr->next, new Node(curr->data, new_next));
+    }
 }
 
-/*
- * Add a node at the front of a linked list.
- * */
-void add_at_front(Node*& head, int d) {
-    head = new Node(d, head);
+Node* duplicate(Node* head){
+    if(!head){
+        return nullptr;
+    }
+    else{
+        if(head->next == nullptr){
+            Node* end_copy = new Node(head->data);
+            return end_copy;
+        }
+        Node* copy_head = new Node(head->data);
+        copy_head->next = duplicate(head->next);
+        return copy_head;
+    }
+}
+
+Node* join(Node*& head, Node* reverse){
+    Node* last_of_head = last(head);
+    last_of_head->next = reverse;
+    return head;
 }
 
 
-/*
- * Get the last node of a list.
- * */
 Node* last(Node* head) {
-    if (!head->next){
+    if(head->next == nullptr){
         return head;
     }
-    else{
-        return last(head->next);
-    }
+    return last(head->next);
 }
--- my_code/llist.h
+++ my_code/llist.h
@@ -18,17 +18,14 @@ struct Node {
  * The various functions that can operate on our linked lists:
  * */
 std::ostream& operator<<(std::ostream& os, const Node* nd);
-void print_list(std::ostream& os, const Node* head);
+void print_list(std::ostream& os, const Node* curr);
 Node* last(Node* head);
-void add_at_end(Node* head, int d);
+void add_at_end(Node*& head, int d);
 void add_at_front(Node*& head, int d);
-
-/*
- * These are coming:
- bool del_head(Node*& head);
- bool del_tail(Node** prev_next);
- Node* duplicate(Node* head);
- Node* reverse(Node* curr, Node* prev=nullptr);
- */
+bool del_head(Node*& head);
+bool del_tail(Node*& head);
+Node* duplicate(Node* head);
+Node* reverse(Node* curr, Node* new_next=nullptr);
+Node* join(Node*& head, Node* reverse);
 
 #endif
