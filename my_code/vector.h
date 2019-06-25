e5712166a8e0695c757316fc7bfcd1cc8505a174
Max Viduszynski <mav456@nyu.edu>
2019-06-25 11:06:51 -0400

    vector iterator

--- my_code/vector.h
+++ my_code/vector.h
@@ -1,8 +1,36 @@
 const int DEF_CAPACITY = 10;
+const int CAPACITY_MULT = 2;
 
 class MyVec {
 public:
+    class Iterator {
+        /*
+         * This class supports ranged for loops.
+         * It includes:
+         * Iterator(int*)
+         * operator++
+         * operator*
+         * operator!=
+         * */
+        friend bool operator!=(Iterator& rhs, Iterator& lhs) {
+            return true;
+        }
+        
+    public:
+        Iterator(int* ip) : iptr(ip) {}
+        
+        Iterator& operator++() {
+            return *this;
+        }
+        
+        int operator*() { return 0; }
+        
+    private:
+        int* iptr;
+    };
+    
     MyVec();
+    MyVec(int sz, int val=0);
     
     // copy control:
     MyVec(const MyVec& v2);
@@ -13,7 +41,11 @@ public:
     int size() const { return sz; }
     int operator[](int i) const;
     int& operator[](int i);
+    Iterator begin() const;
+    Iterator end() const;
+    
 private:
+    void copy(const MyVec& v2);
     int* data;
     int sz;
     int capacity;
