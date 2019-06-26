7b0b994441bdcce7ebfa28af016434be51aed015
Max Viduszynski <mav456@nyu.edu>
2019-06-26 19:29:33 -0400

    Resubmitting Vectors assignment

--- my_code/vector.cpp
+++ my_code/vector.cpp
@@ -1,120 +1,85 @@
-//what does vector add to C arrays
-
-//what is the name of a C array
-
-//a  pointer to the data type thats held by the array
-//calls the address of the begining of the array
-
-
 #include <iostream>
 #include "vector.h"
 
 using namespace std;
 
 void print_vector(const MyVec& v) {
-    for (int i : v) cout << i << " ";
-    cout << endl;
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << ' ';
+    }
 }
 
 
-MyVec::MyVec() : sz(0) {
+MyVec::MyVec() {
+    sz = 0;
     capacity = DEF_CAPACITY;
     data = new int[DEF_CAPACITY];
 }
 
 
-MyVec::MyVec(int sz, int val) : sz{sz} {
-    data = new int[max(DEF_CAPACITY, 2 * sz)];
-    capacity = max(DEF_CAPACITY, 2 * sz);
-    for(int i=0; i < sz; i++){
-        data[i] = val;
-    }
-}
-
 MyVec::MyVec(const MyVec& v2) {
-    copy(v2);
+    capacity = v2.capacity;
+    sz = v2.sz;
+    data = new int[capacity];
+    for(int i=0; i < sz; i++){
+        data[i] = v2[i];
+    }
 }
 
 MyVec::~MyVec() {
     delete [] data;
+    
 }
 
 MyVec& MyVec::operator=(const MyVec& v2) {
-    if (this != &v2) {
-        delete [] data;
-        copy(v2);
+    if (this != &v2){
+        delete[] data;
+        sz = v2.sz;
+        capacity = v2.capacity;
+        data = new int[capacity];
+        for(int i = 0; i < size(); ++i){
+            data[i] = v2.data[i];
+        }
     }
     return *this;
 }
 
 
-MyVec::Iterator MyVec::begin() const {
-    return MyVec::Iterator(data);
-}
-
-MyVec::Iterator MyVec::end() const {
-    return MyVec::Iterator(data + sz);
-}
-
-
-/*
- * == is true when every element of the vectors are the same in
- * the same order. (Thus they must be the same size.)
- * */
 
 bool operator==(MyVec& v1, MyVec& v2) {
-    if ((v1.size() != v2.size())) {
-        cout << "SIZE NOT EQUAL";
+    if(v1.size() != v2.size()){
         return false;
     }
-    for (int i = 0; i < v1.size() ; i++) {
-        if (v1[i] != v2[i]) {
-            cout << "SOMETHING DIFFERENT";
+    for(int i =0; i < v1.size(); i++){
+        if(v1[i] != v2[i]){
             return false;
         }
     }
     return true;
 }
 
-/*
- * Puts an element at the back of a vector.
- * */
+
 void MyVec::push_back(int val) {
-    sz++;
-    if (sz > capacity) {
+    if (sz == capacity) {
         cout << "Increasing capacity\n";
+        capacity *= CAPACITY_MULT;
         int* old_data = data;
-        data = new int[capacity * CAPACITY_MULT];
+        data = new int[capacity];
         for (int i = 0; i < sz; i++) {
             data[i] = old_data[i];
         }
-        capacity *= CAPACITY_MULT;
         delete [] old_data;
     }
-    data[sz - 1] = val;
+    data[sz] = val;
+    sz++;
 }
 
-/*
- * this [] is for reading items from the MyVec:
- * It returns the i-th element.
- * */
+
 int MyVec::operator[](int i) const {
     return data[i];
 }
 
-/*
- * this [] allows write access to items in the MyVec:
- * It returns a reference to the i-th element.
- * */
+
 int& MyVec::operator[](int i) {
     return data[i];
-}
-
-void MyVec::copy(const MyVec& v2) {
-    sz = v2.sz;
-    capacity = v2.capacity;
-    data = new int[capacity];
-    for (int i = 0; i < sz; i++) {
-        data[i] = v2.data[i];
-    }
 }
--- my_code/vector.h
+++ my_code/vector.h
@@ -3,38 +3,9 @@ const int CAPACITY_MULT = 2;
 
 class MyVec {
 public:
-    class Iterator {
-        /*
-         * This class supports ranged for loops.
-         * It includes:
-         * Iterator(int*)
-         * operator++
-         * operator*
-         * operator!=
-         * */
-        friend bool operator!=(Iterator& rhs, Iterator& lhs) {
-            return (rhs.iptr != lhs.iptr);
-        }
-        
-    public:
-        Iterator(int* ip) : iptr(ip) {}
-        
-        Iterator& operator++() {
-            iptr = iptr + 1;
-            return *this;
-        }
-        
-        int operator*() {
-            return *iptr;
-        }
-        
-    private:
-        int* iptr;
-    };
-    
     MyVec();
-    MyVec(int sz, int val=0);
     
+    // copy control:
     MyVec(const MyVec& v2);
     ~MyVec();
     MyVec& operator=(const MyVec& v2);
@@ -43,10 +14,7 @@ public:
     int size() const { return sz; }
     int operator[](int i) const;
     int& operator[](int i);
-    Iterator begin() const;
-    Iterator end() const;
 private:
-    void copy(const MyVec& v2);
     int* data;
     int sz;
     int capacity;
